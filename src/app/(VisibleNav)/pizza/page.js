bbf161c0daa7f0556f6e71897be04e0d8f795ae8
Satyam <saty.kr.jha@gmail.com>
2026-02-27 19:52:08 +0530

    Fast load images

--- src/app/(VisibleNav)/pizza/page.js
+++ src/app/(VisibleNav)/pizza/page.js
@@ -36,11 +36,11 @@ export default function Page() {
             <div className="w-full h-screen grid gap-2 lg:grid-cols-2 grid-rows-2 lg:grid-rows-1 grid-cols-1 lg:pt-5">
                 <div className="w-full h-full flex sm:items-center items-start sm:mt-0 mt-[10%] justify-center ">
                     <div className="relative xl:w-130 xl:h-130 lg:w-120 lg:h-120 sm:w-[55vw] sm:h-[55vw] w-[70vw] h-[70vw]">
-                        {Base && <Image src={`/ingredients/${Base}.png`} loading="lazy" fill alt="pizza" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className=" absolute scale-[1.05]" />}
-                        {Sauce && <Image src={`/ingredients/${Sauce}.png`} loading="lazy" fill alt="pizza" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className=" absolute scale-[0.9]" />}
-                        {Cheese && <Image src={`/ingredients/${Cheese}.png`} loading="lazy" fill alt="pizza" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className=" absolute scale-[0.89]" />}
+                        {Base && <Image src={`/ingredients/${Base}.png`} loading="eager" fill alt="pizza" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className=" absolute scale-[1.05]" />}
+                        {Sauce && <Image src={`/ingredients/${Sauce}.png`} loading="eager" fill alt="pizza" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className=" absolute scale-[0.9]" />}
+                        {Cheese && <Image src={`/ingredients/${Cheese}.png`} loading="eager" fill alt="pizza" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className=" absolute scale-[0.89]" />}
                         {Toppings.length > 0 && Toppings.map((topping, id) =>
-                            <Image key={id} src={`/ingredients/${topping}.png`} loading="lazy" fill alt="pizza" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className=" absolute scale-[0.85]" />
+                            <Image key={id} src={`/ingredients/${topping}.png`} loading="eager" fill alt="pizza" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className=" absolute scale-[0.85]" />
                         )}
                     </div>
                 </div>
